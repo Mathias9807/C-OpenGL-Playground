d6aad61b218148fd740ecda646f6d8c431dc13f9
Mathias9807 <mathias9807@gmail.com>
2015-08-06 17:50:30 +0200

    Cleaned up project.

--- wincompile.bat
+++ wincompile.bat
@@ -1,7 +1,7 @@
-cd build
-gcc ../src/v_*.c ../src/g_*.c ../src/sys_sdl.c ^
-	-I../include ^
+gcc -o 3D.exe src/v_*.c src/g_*.c src/sys_sdl.c ^
+	-I./include ^
 	-DM_PI=3.1415926535 -DGLEW_STATIC -DSDL_MAIN_HANDLED ^
-	-Wall -std=c99 -c
+	-Wall -std=c99 ^
+	-L./winlibs -lSDL2 -lglew32 -lOpenGL32 -lassimp
 
 pause
--- winlink.bat
+++
@@ -1,4 +0,0 @@
-gcc -o 3D build/*.o ^
-	-L./winlibs -lSDL2 -lglew32 -lOpenGL32 -lassimp
-
-pause
