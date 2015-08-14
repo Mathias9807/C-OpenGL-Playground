8e7defe2c95f419193e21e694adb19b8ca83dab6
Mathias9807 <mathias9807@gmail.com>
2015-08-14 11:46:18 +0200

    Removed duplicate Windows DLLs and updated the compile script accordingly.

--- wincompile.bat
+++ wincompile.bat
@@ -2,6 +2,6 @@ gcc -o 3D.exe src/v_*.c src/g_*.c src/sys_sdl.c ^
 	-I./include ^
 	-DM_PI=3.1415926535 -DGLEW_STATIC -DSDL_MAIN_HANDLED ^
 	-Wall -std=c99 ^
-	-L./winlibs -lSDL2 -lglew32 -lOpenGL32 -lassimp
+	-L./winlibs -L. -lSDL2 -lglew32 -lOpenGL32 -lassimp
 
 pause
